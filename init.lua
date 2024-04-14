b00e3dd05309252f546493fed5880c4751dd41bf
its-c10 <caleb.ja.owens@gmail.com>
2024-04-14 09:43:20 -0400

    Need to figure out how to fix comment colors.

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ require("caleb")
 
 vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro" -- Gives line numbers to netr
 vim.wo.relativenumber = true
-
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
 -- [[ Setting options ]]
@@ -137,16 +136,6 @@ require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	-- MY PLUGINS
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		},
-	},
 	"ThePrimeagen/vim-be-good",
 	"nvim-lua/plenary.nvim",
 	{
@@ -490,7 +479,6 @@ require("lazy").setup({
 				-- But for many setups, the LSP (`tsserver`) will work just fine
 				-- tsserver = {},
 				--
-
 				lua_ls = {
 					-- cmd = {...},
 					-- filetypes = { ...},
@@ -691,13 +679,10 @@ require("lazy").setup({
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-moon")
-
-			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
+			vim.cmd.colorscheme("tokyonight")
 		end,
 	},
-
+	"xiyaowong/transparent.nvim",
 	-- Highlight todo, notes, etc in comments
 	{
 		"folke/todo-comments.nvim",
@@ -730,7 +715,7 @@ require("lazy").setup({
 			local statusline = require("mini.statusline")
 			-- set use_icons to true if you have a Nerd Font
 			statusline.setup({ use_icons = vim.g.have_nerd_font })
-
+			--
 			-- You can configure sections in the statusline by overriding their
 			-- default behavior. For example, here we set the section for
 			-- cursor location to LINE:COLUMN
