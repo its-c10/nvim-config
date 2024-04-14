5f5da7d71fec7677ae5a41cf6414110be5d0191b
Caleb <caleb.ja.owens@gmail.com>
2024-04-13 23:30:05 -0400

    committing so that my laptop can have up to date config

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 require("caleb")
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro"
+
+vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro" -- Gives line numbers to netr
 vim.wo.relativenumber = true
 
 -- Set to true if you have a Nerd Font installed
@@ -14,9 +12,6 @@ vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
