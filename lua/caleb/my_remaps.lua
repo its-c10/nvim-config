a4b8c246a3d7ea68e83c03a2e3020ba0a3c6171b
its-c10 <caleb.ja.owens@gmail.com>
2024-04-21 09:03:44 -0400

    New remaps for newlines.

--- lua/caleb/my_remaps.lua
+++ lua/caleb/my_remaps.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- In case I don't want to go into insert mode when I add a new line
+vim.keymap.set("n", "<leader>o", "o<Esc>")
+vim.keymap.set("n", "<leader>O", "O<Esc>")
