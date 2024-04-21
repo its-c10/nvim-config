162b2b695154db36f7fb5509d155596eb949e855
its-c10 <caleb.ja.owens@gmail.com>
2024-04-21 14:55:51 -0400

    Added new delete buffer remap

--- lua/caleb/my_remaps.lua
+++ lua/caleb/my_remaps.lua
@@ -4,3 +4,4 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- In case I don't want to go into insert mode when I add a new line
 vim.keymap.set("n", "<leader>o", "o<Esc><Up>", { desc = "Inserts newline BELOW cursor without going into insert mode and without moving cursor." })
 vim.keymap.set("n", "<leader>O", "O<Esc><Up>", { desc = "Inserts newline ABOVE cursor without going into insert mode and without moving cursor." })
+vim.keymap.set("n", "<leader>x", "<Cmd>:bd<Cr>", { desc = "Deletes the current buffer." })
