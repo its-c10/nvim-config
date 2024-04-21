4ea94581ba750164b461c3f9836ad8c163f4e542
its-c10 <caleb.ja.owens@gmail.com>
2024-04-21 13:26:38 -0400

    improved newline remap

--- lua/caleb/my_remaps.lua
+++ lua/caleb/my_remaps.lua
@@ -2,5 +2,5 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- In case I don't want to go into insert mode when I add a new line
-vim.keymap.set("n", "<leader>o", "o<Esc>")
-vim.keymap.set("n", "<leader>O", "O<Esc>")
+vim.keymap.set("n", "<leader>o", "o<Esc><Up>", { desc = "Inserts newline BELOW cursor without going into insert mode and without moving cursor." })
+vim.keymap.set("n", "<leader>O", "O<Esc><Up>", { desc = "Inserts newline ABOVE cursor without going into insert mode and without moving cursor." })
