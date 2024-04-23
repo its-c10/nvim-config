fdee7332aefb5065ff3cd8e9dfb851b236e81849
its-c10 <caleb.ja.owens@gmail.com>
2024-04-23 11:52:34 -0400

    added copilot

--- init.lua
+++ init.lua
@@ -140,6 +140,7 @@ require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	-- MY PLUGINS
+	"github/copilot.vim",
 	"ThePrimeagen/vim-be-good",
 	"nvim-lua/plenary.nvim",
 	{
